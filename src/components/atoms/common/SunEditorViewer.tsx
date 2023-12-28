efacd0bd1658392e7a24a25e3b5d021de51eb558
kkn1125 <xpfjrnseks1@naver.com>
2023-12-28 15:34:35 +0900

    🌟 add: SunEditor로 제작된 컨텐츠 편집기와 동일한 UI 적용한 뷰어 컴포넌트 제작

---
+++ src/components/atoms/common/SunEditorViewer.tsx
@@ -0,0 +1,33 @@
+import { Box, SxProps, Typography } from "@mui/material";
+import "suneditor/dist/css/suneditor.min.css"; // Import Sun Editor's CSS File
+
+type SunEditorViewerProps = {
+  content: string;
+  sx?: SxProps;
+  wrapSx?: SxProps;
+};
+
+function SunEditorViewer({ content, wrapSx, sx }: SunEditorViewerProps) {
+  return (
+    <Box
+      className='sun-editor'
+      sx={{
+        border: "none",
+        ...wrapSx,
+      }}>
+      <Typography
+        className='se-container sun-editor-editable'
+        variant='body2'
+        dangerouslySetInnerHTML={{
+          __html: content.replace(/<script|^<.*\s?onload=/gm, ""),
+        }}
+        sx={{
+          p: 0,
+          ...sx,
+        }}
+      />
+    </Box>
+  );
+}
+
+export default SunEditorViewer;
