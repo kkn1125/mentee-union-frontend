667a5ef49ec2030c1ee8936a1cbfe5c10fbedbee
kkn1125 <xpfjrnseks1@naver.com>
2023-12-28 15:47:24 +0900

    🛠️ fix: 반응형 사이징 값 수정

--- src/components/templates/UserLayout.tsx
+++ src/components/templates/UserLayout.tsx
@@ -3,7 +3,16 @@ import { Outlet } from "react-router-dom";
 
 function UserLayout() {
   return (
-    <Stack gap={10} sx={{ width: "60%", margin: "auto" }}>
+    <Stack
+      gap={10}
+      sx={{
+        width: {
+          xs: "90%",
+          md: "80%",
+          lg: "70%",
+        },
+        margin: "auto",
+      }}>
       <Outlet />
     </Stack>
   );
