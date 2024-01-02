e46a8117fdfb3112594dea66badfdf10a7d52205
kkn1125 <xpfjrnseks1@naver.com>
2024-01-02 19:06:03 +0900

    🌟 add: 나의 멘티 페이지 나를 추천한 멘티, 내가 추천한 멘티 섹션 컴포넌트 제작 및 모듈화

---
+++ src/components/atoms/user/PointSystemList.tsx
@@ -0,0 +1,33 @@
+import { List, Typography } from "@mui/material";
+import React, { useMemo } from "react";
+
+type pointSystemListProps = {
+  senderList: UserRecommend[];
+  emptyText: string;
+};
+
+function PointSystemList({ senderList, emptyText }: pointSystemListProps) {
+  const removeDuplicatedSender = useMemo(() => {
+    return senderList && senderList.length > 0
+      ? senderList.reduce((acc: User[], cur) => {
+          if (!acc.some((item) => item.id === cur.giver.id)) {
+            acc.push(cur.giver);
+          }
+          return acc;
+        }, [])
+      : [];
+  }, [senderList]);
+
+  return (
+    <List>
+      {removeDuplicatedSender.length === 0 && (
+        <Typography variant='body2'>{emptyText}</Typography>
+      )}
+      {removeDuplicatedSender.map((sender) => (
+        <Typography key={sender.id}>{sender.username}</Typography>
+      ))}
+    </List>
+  );
+}
+
+export default PointSystemList;
