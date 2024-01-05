93befdb896629b9af0be9733fa9a48e1ccde7c95
kkn1125 <xpfjrnseks1@naver.com>
2024-01-05 16:27:46 +0900

    🐛 bugfix: typescript 유형 정의 에러로 styled 컴포넌트에서 mui 단일 컴포넌트로 변경

--- src/components/atoms/common/VisuallyHiddenInput.tsx
+++ src/components/atoms/common/VisuallyHiddenInput.tsx
@@ -1,13 +1,23 @@
-import { styled } from "@mui/material";
+import { Box } from "@mui/material";
 
-export default styled("input")({
-  clip: "rect(0 0 0 0)",
-  clipPath: "inset(50%)",
-  height: 1,
-  overflow: "hidden",
-  position: "absolute",
-  bottom: 0,
-  left: 0,
-  whiteSpace: "nowrap",
-  width: 1,
-});
+const VisuallyHiddenInput = (props: any) => {
+  return (
+    <Box
+      {...props}
+      component='input'
+      sx={{
+        clip: "rect(0 0 0 0)",
+        clipPath: "inset(50%)",
+        height: 1,
+        overflow: "hidden",
+        position: "absolute",
+        bottom: 0,
+        left: 0,
+        whiteSpace: "nowrap",
+        width: 1,
+      }}
+    />
+  );
+};
+
+export default VisuallyHiddenInput;
