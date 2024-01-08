867e1c868c95365314cb910df771761d6b485512
kkn1125 <xpfjrnseks1@naver.com>
2024-01-08 18:32:53 +0900

    ✨ feat: landing page cover image 추가

--- src/components/organisms/home/BackgroundSection.tsx
+++ src/components/organisms/home/BackgroundSection.tsx
@@ -1,7 +1,7 @@
 import { TokenContext } from "@/context/TokenProvider";
 import { CHECK_MESSAGE } from "@/util/global.constants";
-import { Box, Typography, Button } from "@mui/material";
-import React, { useContext } from "react";
+import { Box, Button, Typography } from "@mui/material";
+import { useContext } from "react";
 import { useNavigate } from "react-router-dom";
 
 interface BackgroundSectionProps {
@@ -17,22 +17,41 @@ function BackgroundSection({ src }: BackgroundSectionProps) {
       sx={{
         width: "100%",
         height: "75vh",
-        backgroundImage: `url(${
-          src || "https://www.industrialempathy.com/img/remote/ZiClJf-1920w.jpg"
-        })`,
-        backgroundSize: "cover",
-        backgroundPosition: "center",
+        position: "relative",
         display: "flex",
         flexDirection: "column",
         justifyContent: "center",
         alignItems: "center",
         textAlign: "center",
-        color: "white",
+        color: "#232323",
+        backgroundColor: "#b7dbdf",
       }}>
-      <Typography variant='h3' gutterBottom>
+      <Box
+        sx={{
+          backgroundImage: `url(${
+            src || "/assets/cover/dalle-mentee-union-main-cover-edit.png"
+          })`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center center",
+          filter: "blur(1.5px)",
+          width: "100%",
+          height: "100%",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          zIndex: 0,
+        }}
+      />
+      <Typography
+        variant='h3'
+        gutterBottom
+        sx={{
+          zIndex: 1,
+        }}>
         성장을 함께하는 멘티 커뮤니티
       </Typography>
-      <Typography variant='h6' sx={{ px: 3 }}>
+      <Typography variant='h6' sx={{ px: 3, zIndex: 1 }}>
         멘티들이 모여 세미나와 지식을 공유하며, 멘토링 기회를 얻는 곳입니다.
       </Typography>
       <Button
